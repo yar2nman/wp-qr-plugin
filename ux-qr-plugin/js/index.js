5822011edd0693037001079d9d868d91c88e4a87
Ahmed7779 <yar2nman@gmail.com>
2022-06-19 09:41:04 +0300

    Print to PDF Print a selected div or other element to a pdf file

--- ux-qr-plugin/js/index.js
+++ ux-qr-plugin/js/index.js
@@ -49,3 +49,14 @@ $(document).ready(function(){
     }
 
 })
+
+function PrintToPDF(divSelector) {
+    var divContents = $(divSelector).html();
+    var printWindow = window.open('', '', 'height=400,width=800');
+    printWindow.document.write('<html><head><title>DIV Contents</title>');
+    printWindow.document.write('</head><body >');
+    printWindow.document.write(divContents);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print();
+};
