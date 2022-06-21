7b5f3ce4c691f58f4fca0591646faec53b699fab
Ahmed7779 <yar2nman@gmail.com>
2022-06-21 08:15:42 +0300

    export to excel export to csv
    created two functions to eport table to excell r export data to csv file

--- ux-qr-plugin/js/index.js
+++ ux-qr-plugin/js/index.js
@@ -51,7 +51,7 @@ $(document).ready(function(){
 })
 
 function PrintToPDF(divSelector) {
-    // divSelecttor = "#divId"
+    // divSelector = "#divId"
     var divContents = $(divSelector).html();
     var printWindow = window.open('', '', 'height=400,width=800');
     printWindow.document.write('<html><head><title>DIV Contents</title>');
@@ -61,3 +61,63 @@ function PrintToPDF(divSelector) {
     printWindow.document.close();
     printWindow.print();
 };
+
+function exportTableToExcel(tableID, filename = ''){
+    // to use the function
+    // <button onclick="exportTableToExcel('tableID', 'excelFileName')">Export Table Data To Excel File</button>
+    var downloadLink;
+    var dataType = 'application/vnd.ms-excel';
+    var tableSelect = document.getElementById(tableID);
+    var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+    
+    // Specify file name
+    filename = filename?filename+'.xls':'excel_data.xls';
+    
+    // Create download link element
+    downloadLink = document.createElement("a");
+    
+    document.body.appendChild(downloadLink);
+    
+    if(navigator.msSaveOrOpenBlob){
+        var blob = new Blob(['\ufeff', tableHTML], {
+            type: dataType
+        });
+        navigator.msSaveOrOpenBlob( blob, filename);
+    }else{
+        // Create a link to the file
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+    
+        downloadLink.download = filename;
+        
+        downloadLink.click();
+    }
+}
+
+function exportDataToCSV(data, fileName = 'dataFile') {
+
+    let csvContent = "data:text/csv;charset=utf-8,";
+    
+    data.forEach(function(rowArray) {
+        let row = rowArray.join(",");
+        csvContent += row + "\r\n";
+    });
+
+    var encodedUri = encodeURI(csvContent);
+
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", fileName + ".csv");
+    document.body.appendChild(link);
+
+    link.click();
+
+    /*
+        // Usage of exportDataToCSV function
+        const rows = [
+            ["name1", "city1", "some other info"],
+            ["name2", "city2", "more info"]
+        ];
+
+        exportDataToCSV(rows, "DataFileName");
+    */
+}
